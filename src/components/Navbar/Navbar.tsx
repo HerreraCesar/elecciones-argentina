0de1ae8be5fc2dc445226f2087e860f1aa3e2744
César <herrera.cesar.arg@outlook.com>
2023-04-22 17:07:23 -0300

    Menu styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
-import styles from "./Navbar.module.scss";
-import { t } from "~/helpers/translate";
-import { useApplicationContext } from "~/context/context";
-import { A } from "solid-start";
-import { FaSolidPlus } from "solid-icons/fa";
-import { createSignal } from "solid-js";
+import { A } from 'solid-start';
+import { FaSolidPlus } from 'solid-icons/fa';
+import { createSignal } from 'solid-js';
+import styles from './Navbar.module.scss';
+import { t } from '~/helpers/translate';
+import { useApplicationContext } from '~/context/context';
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = createSignal(false);
@@ -12,39 +12,44 @@ export default function Navbar() {
   const [language, { changeLanguage }] = store.language;
 
   return (
-    <header class={styles.header}>
-      <nav class={styles.navbar}>
-        <ul>
-          <li>
-            <A href="/home" class={styles.link} activeClass={styles.active}>
-              {t("logo")}
-            </A>
-          </li>
-          <li>
-            <A href="/timeline" class={styles.link} activeClass={styles.active}>
-              {t("timeline")}
-            </A>
-          </li>
-        </ul>
-      </nav>
-      <div class={styles.button} onClick={() => setMenuOpen(!menuOpen())}>
-        <FaSolidPlus />
-        <div class={`${styles.square} ${menuOpen() && styles.open}`}>
-          <div class={styles.one}></div>
-          <div class={styles.two}></div>
-          <div class={styles.three}></div>
-          <div class={styles.four}></div>
-          <div class={styles.five}></div>
-          <div class={styles.six}></div>
-          <div class={styles.seven}></div>
-          <div class={styles.eight}></div>
-          <div class={styles.nine}></div>
+    <>
+      <header class={styles.header}>
+        <nav class={styles.navbar}>
+          <ul>
+            <li>
+              <A href="/home" class={styles.link} activeClass={styles.active}>
+                {t('logo')}
+              </A>
+            </li>
+            <li>
+              <A
+                href="/timeline"
+                class={styles.link}
+                activeClass={styles.active}
+              >
+                {t('timeline')}
+              </A>
+            </li>
+          </ul>
+        </nav>
+        <div class={`${menuOpen() ? styles.touched_button : styles.button}`}>
+          <FaSolidPlus onClick={() => setMenuOpen(!menuOpen())} />
+          <div class={`${menuOpen() ? styles.open_menu : styles.close_menu}`}>
+            <div class={styles.card}>
+              <button onClick={() => changeLanguage('es')}>ES</button>
+              <button onClick={() => changeLanguage('en')}>EN</button>
+            </div>
+            <div class={styles.card}>
+              <button onClick={() => changeTheme('dark')}>dark</button>
+              <button onClick={() => changeTheme('light')}>light</button>
+            </div>
+          </div>
         </div>
-        {/* <button onClick={() => changeLanguage("es")}>ES</button>
-        <button onClick={() => changeLanguage("en")}>EN</button>
-        <button onClick={() => changeTheme("dark")}>dark</button>
-        <button onClick={() => changeTheme("light")}>light</button> */}
-      </div>
-    </header>
+      </header>
+      <div
+        class={`${menuOpen() ? styles.overlay : styles.close_overlay}`}
+        onClick={() => setMenuOpen(!menuOpen())}
+      />
+    </>
   );
 }
